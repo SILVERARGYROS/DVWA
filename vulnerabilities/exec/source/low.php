4c7cb9b2294e6417b430b86b15fd062acca94227
SILVERARGYROS <76134085+SILVERARGYROS@users.noreply.github.com>
2022-12-12 14:27:53 +0200

    Update low.php
    - Fixed vulnerability
    Used 'escapeshellarg' function on user input to escape possible shell meta-arguments

--- vulnerabilities/exec/source/low.php
+++ vulnerabilities/exec/source/low.php
@@ -2,7 +2,7 @@
 
 if( isset( $_POST[ 'Submit' ]  ) ) {
 	// Get input
-	$target = $_REQUEST[ 'ip' ];
+	$target = escapeshellarg($_REQUEST[ 'ip' ]);
 
 	// Determine OS and execute the ping command.
 	if( stristr( php_uname( 's' ), 'Windows NT' ) ) {
