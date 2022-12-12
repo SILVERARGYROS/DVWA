19d1953d4ff199dce11a86398b3b7b037f2337fe
SILVERARGYROS <76134085+SILVERARGYROS@users.noreply.github.com>
2022-12-12 13:58:01 +0200

    Update jsonp.php
    - Fixed vulnerability.
    
    User input should always be considered untrusted first.

--- vulnerabilities/csp/source/jsonp.php
+++ vulnerabilities/csp/source/jsonp.php
@@ -3,11 +3,12 @@ header("Content-Type: application/json; charset=UTF-8");
 
 if (array_key_exists ("callback", $_GET)) {
 	$callback = $_GET['callback'];
+	$safecallback = htmlspecialchars($callback);
 } else {
 	return "";
 }
 
 $outp = array ("answer" => "15");
 
-echo $callback . "(".json_encode($outp).")";
+echo $safecallback . "(".json_encode($outp).")";
 ?>
